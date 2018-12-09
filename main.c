140de9a5ebbeddc06fc1f6528d963012c9145af2
Minhyung Kim <alsgud3229@naver.com>
2018-12-10 03:31:46 +0900

    all passed except for 'write-back'
    TODO: write-back

--- main.c
+++ main.c
@@ -10,6 +10,8 @@
 /*
  * Cache structures
  */
+
+ // cap:way:block size
 int time = 0;
 
 typedef struct {
@@ -44,7 +46,7 @@ cache build_cache(int set, int capacity, int way, int blocksize) {
 	// cache[set][assoc][word per block]
 	//32비트가 워드 하나로 고정임 항상
 	cache myCache;
-	myCache.E = index_bit(way); //
+	myCache.E = way; //
 	myCache.s = index_bit(set); //index bit
 	myCache.b = index_bit(blocksize); //offset bit
 
@@ -54,12 +56,161 @@ cache build_cache(int set, int capacity, int way, int blocksize) {
 		myCache.sets[i].lines = (cline *)malloc(way*sizeof(cline));
 	}
 
-	// Cache.sets[0].lines[0].tag = 0x00000002; 이런식의 assign 가능
-	
+	for(int i=0; i<set; i++){
+		for(int j=0; j<way; j++){
+			myCache.sets[i].lines[j].valid=0;
+			myCache.sets[i].lines[j].age=0;
+			myCache.sets[i].lines[j].tag=0;
+			myCache.sets[i].lines[j].modified=0;
+		}
+	}
+
 	return myCache;
 }
 
-void access_cache() {
+void free_cache(cache *myCache, int set, int way){
+
+}
+
+void access_cache(cache *myCache, char *op, uint32_t addr,
+ int *read, int *write, 
+ int* writeback, int*readhit, int*writehit, int*readmiss, int*writemiss) {
+	// total reads
+	// total writes
+	// write-backs
+	//read hits
+	//write hits
+	//read misses
+	//write misses
+	int oldest = 0;
+	int victim = 0;
+	int s = myCache->s; // index bit
+	int b = myCache->b;// blocksize
+	int way = myCache->E;
+	// tag_bit = 32-s-b
+	uint32_t tag_from_data = addr >> (s+b);
+	uint32_t index = addr;
+	index = index >> b;
+	index = index & 0xF;
+
+	// printf("%d\n", myCsache->sets[index].lines[0].valid);
+
+
+// write_back 구현 안한거
+		
+	if(strcmp(op, "R")==0){				// read 일때 
+		*read += 1;
+		for(int j=0; j<way; j++){ // 그 인덱스를 살피는 것
+
+			if(myCache->sets[index].lines[j].valid == 1){ // 있는놈이랑 
+				if(myCache->sets[index].lines[j].tag == tag_from_data){ // 태그 같으면 hit, 다르면 continue
+					*readhit += 1;
+					myCache->sets[index].lines[j].age = time;
+					break;
+				}
+			}
+			if(myCache->sets[index].lines[j].valid == 0){ // 빈자리 발견
+				*readmiss += 1;
+				myCache->sets[index].lines[j].valid = 1;
+				myCache->sets[index].lines[j].modified=0;
+				myCache->sets[index].lines[j].age = time;
+				myCache->sets[index].lines[j].tag = tag_from_data;
+				break;
+			}
+			if(j==way-1){ //다 돌았는데 태그도 다 다르고 빈자리가 없는것
+				oldest = time - (myCache->sets[index].lines[0].age);
+				for(int i=0; i<way; i++){
+					if(oldest < time - (myCache->sets[index].lines[i].age)){
+						oldest = time - (myCache->sets[index].lines[i].age);
+						victim=i;												// victim selection
+					}
+				}
+				*readmiss += 1;
+				myCache->sets[index].lines[victim].valid = 1;    //victim eviction
+				myCache->sets[index].lines[victim].age = time;
+				myCache->sets[index].lines[victim].tag = tag_from_data;
+				if(myCache->sets[index].lines[victim].modified==1){
+					*writeback += 1;
+					myCache->sets[index].lines[victim].modified = 0;
+				}
+				break;
+			}
+		}
+
+		// printf("i survived\n");
+
+	}
+
+
+/*
+index 비교, 그 인덱스에 누가 있는지? 
+	누구라도 있으면 태그비교
+		태그같다면
+			(writehit), modified=1
+		다르다면
+			skip
+	빈자리 있으면
+		바로 넣음 --> write miss, valid, age, tag, modified
+	빈자리 없으면
+		교체 --> writemiss, valid, age, tag, modified
+
+*/
+
+	else{			//////       wirte 일때
+		*write += 1;
+		for(int j=0; j<way; j++){ // 그 인덱스를 살피는 것
+
+			if(myCache->sets[index].lines[j].valid == 1){ // 있는놈이랑 
+				if(myCache->sets[index].lines[j].tag == tag_from_data){ // 태그 같으면 hit, 다르면 continue
+					*writehit += 1;
+					myCache->sets[index].lines[j].modified=1;
+					myCache->sets[index].lines[j].age = time;
+					break;
+				}
+			}
+			if(myCache->sets[index].lines[j].valid == 0){ // 빈자리 발견
+				*writemiss += 1;
+				myCache->sets[index].lines[j].valid = 1;
+				myCache->sets[index].lines[j].age = time;
+				myCache->sets[index].lines[j].tag = tag_from_data;
+				myCache->sets[index].lines[j].modified=1;
+				break;
+			}
+			if(j==way-1){ //다 돌았는데 태그도 다 다르고 빈자리가 없는것
+				oldest = time - (myCache->sets[index].lines[0].age);
+				for(int i=0; i<way; i++){
+					if(oldest < time - (myCache->sets[index].lines[i].age)){
+						oldest = time - (myCache->sets[index].lines[i].age);
+						victim=i;												// victim selection
+					}
+				}
+				*writemiss += 1;													//victim eviction
+				myCache->sets[index].lines[victim].modified = 1;
+				myCache->sets[index].lines[victim].valid = 1;
+				myCache->sets[index].lines[victim].age = time;
+				myCache->sets[index].lines[victim].tag = tag_from_data;
+				if(myCache->sets[index].lines[victim].modified==1){
+					*writeback += 1;
+					myCache->sets[index].lines[victim].modified = 1;
+				}
+
+				break;
+			}
+		}
+	}
+
+
+	// for(int j=0; j<way; j++){
+	// 	if(myCache->sets[index].lines[j].valid == 0){
+	// 		printf("its empty\n");
+	// 	}
+	// }
+
+	// printf("index: 0x%x\n", addr);
+
+
+
+
 
 }
 
@@ -237,7 +388,7 @@ int main(int argc, char *argv[]) {
     /* TODO: Define a cache based on the struct declaration */
     simCache = build_cache(set, capacity, way, blocksize);
 
-    //printf("tag: 0x%x\n", simCache.sets[0].lines[0].tag);  이런식 출력 가능
+    //printf("tag: 0x%x\n", simCache.sets[4].lines[1].tag);
 
 	// simulate
 	fp = fopen(trace_name, "r"); // read trace file
@@ -257,8 +408,8 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "op: %s\n", op);
         fprintf(stderr, "addr: %x\n", addr);
 #endif
-        // ...
-        // access_cache()
+        time++;
+        access_cache(&simCache, op, addr, &read, &write, &writeback, &readhit, &writehit, &readmiss, &writemiss);
         // ...
     }
 
@@ -268,5 +419,7 @@ int main(int argc, char *argv[]) {
 	    	xdump(&simCache);
 	}
 
-    	return 0;
+	// free_cache(&simCache, set ,way);
+
+    return 0;
 }
